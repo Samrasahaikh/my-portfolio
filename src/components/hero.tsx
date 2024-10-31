5976036afbac84e82cb8c481652137e7a315ef37
Samrashaikh <shaikhsamra855@gmail.com>
2024-10-31 23:48:04 +0500

    changes code

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
   return (
   // components/Hero.tsx
 
-    <section className="h-screen flex flex-col-reverse md:flex-row items-center justify-between px-4 md:px-8 max-w-screen-2xl"
+    <section className="h-screen min-h-screen flex flex-col-reverse md:flex-row items-center justify-between px-4 md:px-8 max-w-screen-2xl"
     style={{ backgroundImage: "url('/bg2.jpg')",backgroundSize: "cover" }}>
       {/* Right section with text */}
       <div className="text-center py-20">
@@ -12,15 +12,16 @@ export default function Hero() {
 
       </div>
       {/* Left section with image */}
-      <div className="flex-shrink-0 mb-10 md:mb-0 py-10 animate-slideIn animate-fadeIn transform transition-transform duration-1000 ease-in-out hover:scale-110">
+      
         <Image 
           src="/img7.png" 
           alt="Profile Picture"
           width={300}
           height={300}
-          className="w-80 h-80 md:w-80 md:h-80 object-fill rounded-full border-4 border-pink-600"
+          className="w-80 h-80 md:w-80 md:h-80 object-center rounded-full border-4 border-pink-800 mb-10 mt-8 md:mb-0
+          transform transition-transform duration-500 ease-in-out hover:scale-110 animate-fadeIn"
         />
-      </div>
+      
     </section>
 
   )
